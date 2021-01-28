2b729b05a991616cd94b71e2be1252fad17cf658
Robson Puert <rob_puert@hotmail.com>
2021-01-28 00:46:49 -0300

    Endpoint /api, /api/:id

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => res.send('Projeto Inicial Rob'));
+app.get('/api', (req, res) => res.json({Nome: 'Rob', Sobrenome: 'Puert'}));
+
+app.get('/api/:id', (req, res) => {
+  const data = {
+    Nome: 'Rob',
+    Sobrenome: 'Puert',
+    id: req.params.id
+  }
+
+  res.json(data)
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server rodando no caminho: http://localhost:${PORT}`);
